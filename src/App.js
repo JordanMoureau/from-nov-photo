a0bf4683e1988af14d3346f3fa7ee7afa5e3b22b
Jordan Moureau <jordanfromnovember@gmail.com>
2024-05-30 22:15:42 -0700

    max width fix

--- src/App.js
+++ src/App.js
@@ -8,16 +8,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="portfolio" element={<Portfolio />} />
-          <Route path="about" element={<About />} />
-          <Route path="book" element={<Book />} />
-        </Routes>
-      </BrowserRouter>
-      <Footer />
+    <div className="outer">
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="portfolio" element={<Portfolio />} />
+            <Route path="about" element={<About />} />
+            <Route path="book" element={<Book />} />
+          </Routes>
+        </BrowserRouter>
+        <Footer />
+      </div>
     </div>
   );
 }
