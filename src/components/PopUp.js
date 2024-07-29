935ce7df1bc240d98e8e1e521e2e947e8b9adff1
Jordan Moureau <jordanfromnovember@gmail.com>
2024-07-29 15:40:01 -0700

    popup window fixes image width over

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -9,7 +9,7 @@ function PopUp() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowPopUp(true);
-    }, 4000);
+    }, 6000);
 
     return () => clearTimeout(timer);
   }, []);
